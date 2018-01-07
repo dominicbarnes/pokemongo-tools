14c38056c067c7bb57fc7c208c258f20c00c369c
Dominic Barnes <dominic@dbarnes.info>
2018-01-06 23:17:20 -0800

    perf: stop inlining pokesprite image

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,11 @@ const { env } = require('./config')
 
 let plugins = [
   require('postcss-import')(),
-  require('postcss-url')({ url: 'inline' })
+  require('postcss-url')({
+    url: 'copy',
+    assetsPath: 'assets',
+    useHash: true
+  })
 ]
 
 if (env !== 'development') {
