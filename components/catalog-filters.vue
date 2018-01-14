6f291cd2210dc8e2e174b7d05993fbd8e0dfb187
Dominic Barnes <dominic@dbarnes.info>
2018-01-14 00:17:44 -0800

    feat(catalog): use horizontal filters, add 'does evolve' filter

---
+++ components/catalog-filters.vue
@@ -0,0 +1,98 @@
+<template>
+  <b-navbar variant="light" toggleable>
+    <b-nav-text class="mr-2">Filters:</b-nav-text>
+    <b-nav-form>
+      <b-form-input v-model="value.keywords" size="sm" type="text" placeholder="Keywords" />
+    </b-nav-form>
+    <b-navbar-toggle target="catalog-filters" />
+    <b-collapse is-nav id="catalog-filters">
+      <b-navbar-nav>
+        <b-nav-item-dropdown>
+          <template slot="button-content">
+            IVs
+            <span v-if="value.minIV > 0">(&ge;{{value.minIV}}%)</span>
+          </template>
+          <b-dropdown-header>Class</b-dropdown-header>
+          <b-dropdown-item v-on:click="value.minIV = 100">S (100%)</b-dropdown-item>
+          <b-dropdown-item v-on:click="value.minIV = 90">A (&ge;90%)</b-dropdown-item>
+          <b-dropdown-item v-on:click="value.minIV = 80">B (&ge;80%)</b-dropdown-item>
+          <b-dropdown-item v-on:click="value.minIV = 70">C (&ge;70%)</b-dropdown-item>
+          <b-dropdown-item v-on:click="value.minIV = 60">D (&ge;60%)</b-dropdown-item>
+          <b-dropdown-item v-on:click="value.minIV = 50">F (&ge;50%)</b-dropdown-item>
+          <b-dropdown-item v-on:click="value.minIV = 0">Show All</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item-dropdown>
+          <template slot="button-content">
+            Type(s)
+            <type-badge v-for="type in value.types" v-bind:type="type" />
+          </template>
+          <b-dropdown-item v-for="type in types" v-on:click="toggleType(type)" v-bind:active="isActiveType(type)" v-bind:disabled="isDisabledType(type)">
+            <type-badge v-bind:type="type" />
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item-dropdown>
+          <template slot="button-content">
+            <span v-if="value.evolves !== null">{{value.evolves ? 'Does Evolve' : 'Does Not Evolve'}}</span>
+            <span v-else>Evolves?</span>
+          </template>
+          <b-dropdown-item v-on:click="value.evolves = true">Yes</b-dropdown-item>
+          <b-dropdown-item v-on:click="value.evolves = false">No</b-dropdown-item>
+          <b-dropdown-item v-on:click="value.evolves = null">Show All</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+  import TypeBadge from './type-badge.vue'
+
+  export default {
+    data() {
+      return {
+        value: {
+          keywords: '',
+          minIV: 0,
+          types: [],
+          evolves: null
+        }
+      }
+    },
+
+    computed: {
+      types() {
+        return this.$store.state.metadata.types.list
+      }
+    },
+
+    methods: {
+      toggleType(type) {
+        const index = this.value.types.indexOf(type)
+        if (index > -1) {
+          this.value.types.splice(index, 1)
+        } else {
+          this.value.types.push(type)
+        }
+      },
+
+      isActiveType(type) {
+        return this.value.types.indexOf(type) > -1
+      },
+
+      isDisabledType(type) {
+        return !this.isActiveType(type) && this.value.types.length >= 2
+      }
+    },
+
+    watch: {
+      value: {
+        deep: true,
+        handler() {
+          this.$emit('input', this.value)
+        }
+      }
+    },
+
+    components: { TypeBadge }
+  }
+</script>
--- components/catalog.vue
+++ components/catalog.vue
@@ -1,48 +1,44 @@
 <template>
-  <b-container fluid>
-    <b-row>
-      <b-col cols="2" class="p-3 bg-light">
-        <catalog-filter v-model="filters" />
-      </b-col>
-      <b-col class="p-3">
-        <b-row>
-          <b-col class="py-2">
-            Showing
-            <b-badge>{{ currentPageRows.length }}</b-badge>
-            of
-            <b-badge>{{ totalRows }}</b-badge>
-          </b-col>
-          <b-col>
-            <b-pagination v-bind:total-rows="totalRows" v-bind:per-page="perPage" v-model="currentPage" align="center" />
-          </b-col>
-          <b-col class="text-right">
-            <b-button v-bind:to="{ name: 'catalog-add' }" variant="primary">Add Pokémon</b-button>
-          </b-col>
-        </b-row>
-        <b-table v-bind:fields="fields" v-bind:items="items" v-bind:filter="filter" v-bind:per-page="perPage" v-bind:current-page="currentPage" v-on:filtered="updatePagination" v-model="currentPageRows" striped hover show-empty>
-          <template slot="icon" scope="data">
-            <pokesprite v-if="data.item.dex" v-bind:pokemon="data.item.dex" v-bind:shiny="data.item.shiny" v-bind:gender="data.item.gender" />
-          </template>
-          <template slot="name" scope="data">
-            <b-link v-bind:to="{ name: 'catalog-view', params: { pokemon: data.item.id } }" v-bind:title="data.item.notes">{{ data.item.name }}</b-link>
-            <em v-if="data.item.notes">*</em>
-          </template>
-          <template slot="dex" scope="data">
-            {{ data.item.dex | dex }}
-          </template>
-          <template slot="ivs" scope="data">
-            {{ data.item.ivs / 45 | percentage }}
-          </template>
-          <template slot="types" scope="data">
-            <type-badge v-for="type in data.item.types" v-bind:type="type" />
-          </template>
-          <template slot="added" scope="data">
-            <rel-time v-bind:datetime="data.item.added" refresh="1m" />
-          </template>
-        </b-table>
-      </b-col>
-    </b-row>
-  </b-container>
+  <div>
+    <catalog-filters v-model="filters" />
+    <b-container fluid class="pt-2">
+      <b-row>
+        <b-col class="py-2">
+          Showing
+          <b-badge>{{ currentPageRows.length }}</b-badge>
+          of
+          <b-badge>{{ totalRows }}</b-badge>
+        </b-col>
+        <b-col>
+          <b-pagination v-bind:total-rows="totalRows" v-bind:per-page="perPage" v-model="currentPage" align="center" />
+        </b-col>
+        <b-col class="text-right">
+          <b-button v-bind:to="{ name: 'catalog-add' }" variant="primary">Add Pokémon</b-button>
+        </b-col>
+      </b-row>
+      <b-table v-bind:fields="fields" v-bind:items="items" v-bind:filter="filter" v-bind:per-page="perPage" v-bind:current-page="currentPage" v-on:filtered="updatePagination" v-model="currentPageRows" striped hover show-empty>
+        <template slot="icon" scope="data">
+          <pokesprite v-if="data.item.dex" v-bind:pokemon="data.item.dex" v-bind:shiny="data.item.shiny" v-bind:gender="data.item.gender" />
+        </template>
+        <template slot="name" scope="data">
+          <b-link v-bind:to="{ name: 'catalog-view', params: { pokemon: data.item.id } }" v-bind:title="data.item.notes">{{ data.item.name }}</b-link>
+          <em v-if="data.item.notes">*</em>
+        </template>
+        <template slot="dex" scope="data">
+          {{ data.item.dex | dex }}
+        </template>
+        <template slot="ivs" scope="data">
+          {{ data.item.ivs / 45 | percentage }}
+        </template>
+        <template slot="types" scope="data">
+          <type-badge v-for="type in data.item.types" v-bind:type="type" />
+        </template>
+        <template slot="added" scope="data">
+          <rel-time v-bind:datetime="data.item.added" refresh="1m" />
+        </template>
+      </b-table>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -52,7 +48,7 @@
   import Pokesprite from './pokesprite.vue'
   import RelTime from './rel-time.vue'
   import TypeBadge from './type-badge.vue'
-  import CatalogFilter from './catalog-filter.vue'
+  import CatalogFilters from './catalog-filters.vue'
 
   export default {
     data() {
@@ -98,24 +94,35 @@
             ivs: totalIVs,
             name: name,
             notes: pokemon.notes,
+            pokemon: pokemon.pokemonID,
             shiny: pokemon.shiny,
             types: metadata.types
           }
         })
+      },
+
+      evolves() {
+        return this.$store.state.metadata.pokemon
+          .filter(pokemon => pokemon.nextEvolutions.length > 0)
+          .map(pokemon => pokemon._id)
       }
     },
 
     methods: {
       filter(row) {
-        const { keywords, minIV, types } = this.filters
+        const { evolves, keywords, minIV, types } = this.filters
 
         if (minIV) {
           const actualIV = (row.ivs / 45) * 100
-          if (actualIV < minIV) return false
+          if (isNaN(actualIV) || actualIV < minIV) return false
         }
 
         if (types && types.length) {
           if (types.some(type => row.types.indexOf(type) === -1)) return false
+        }
+
+        if (typeof evolves === 'boolean') {
+          if (evolves !== this.evolves.indexOf(row.pokemon) > -1) return false
         }
 
         if (keywords) {
@@ -131,6 +138,6 @@
       }
     },
 
-    components: { Pokesprite, RelTime, TypeBadge, CatalogFilter }
+    components: { Pokesprite, RelTime, TypeBadge, CatalogFilters }
   }
 </script>
