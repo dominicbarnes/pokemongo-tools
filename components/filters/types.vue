449417f3b4ec0565e28474b9fc53fe6ddbcc1911
Dominic Barnes <dominic@dbarnes.info>
2018-05-28 10:56:58 -0700

    feat(filters): convert types to checkbox + badge

--- components/filters/types.vue
+++ components/filters/types.vue
@@ -1,6 +1,10 @@
 <template>
   <b-form-group label="Type(s)">
-    <b-form-select multiple v-bind:options="options" v-bind:value="value" v-bind:size="size" v-on:change="update" />
+    <b-form-checkbox-group stacked v-on:change="update">
+      <b-form-checkbox v-for="type in types" v-bind:value="type" v-bind:checked="isChecked(type)">
+        <type-badge v-bind:type="type" />
+      </b-form-checkbox>
+    </b-form-checkbox-group>
   </b-form-group>
 </template>
 
@@ -16,19 +20,19 @@ export default {
     types() {
       return this.$store.state.metadata.types.list
     },
-
-    size() {
-      return this.types.length.toString()
-    },
-
-    options() {
-      return this.types.map(type => {
-        return { value: type, text: type }
-      })
+    checked() {
+      const s = new Set()
+      for (const t of this.value) {
+        s.add(t)
+      }
+      return s
     }
   },
 
   methods: {
+    isChecked(type) {
+      return this.checked.has(type)
+    },
     update(value) {
       this.$emit('input', value)
     }
