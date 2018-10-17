649397776a5aca8fb0cf9318ae5c6f9273f94026
Dominic Barnes <dominic@dbarnes.info>
2018-10-16 21:40:02 -0700

    dev: add script for only mining local data

--- scripts/mine.js
+++ scripts/mine.js
@@ -5,9 +5,6 @@ const miner = require('./miner')
 main().catch(err => console.error(err))
 
 async function main () {
-  await couchdb.drop()
-  await couchdb.pull()
   await miner()
   await couchdb.update()
-  console.log('local database ready, you can now perform the manual push operation')
 }
