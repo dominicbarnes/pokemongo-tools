eed4305e70f76b233e0ade3db2cebbf20a32626c
Dominic Barnes <dominic@dbarnes.info>
2018-10-18 00:12:04 -0700

    refactor: remove unused slugify dep

--- utils.js
+++ utils.js
@@ -1,23 +1,14 @@
+
 import numeral from 'numeral'
-import slugify from 'slugify'
-
-slugify.extend({
-  '♀': 'f',
-  '♂': 'm'
-})
-
-exports.dex = function (number) {
-  return `#${numeral(number).format('000')}`
-}
-
-exports.slugify = function (name) {
-  return slugify(name, { lower: true })
-}
 
 exports.page = function (list, currentPage, perPage) {
   const start = (currentPage - 1) * perPage
   const end = start + perPage
   return list.slice(start, end)
+}
+
+exports.dex = function (number) {
+  return `#${numeral(number).format('000')}`
 }
 
 exports.cp = function (attack, defense, stamina, multiplier) {
