e94e3bfd2c557d345859e90c4d92c524186f40c4
Dominic Barnes <dominic@dbarnes.info>
2018-11-20 23:53:39 -0800

    feat(service-worker): upgrade to workbox@3, add caching for icons

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,14 @@
-importScripts('https://unpkg.com/workbox-sw@2.1.1/build/importScripts/workbox-sw.prod.v2.1.1.js')
-
-const workbox = new WorkboxSW({ clientsClaim: true })
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js')
 
 const staleWhileRevalidate = workbox.strategies.staleWhileRevalidate()
 const networkOnly = workbox.strategies.networkOnly()
+
+workbox.clientsClaim()
 
 workbox.router.registerRoute('/', staleWhileRevalidate) // skeleton
 workbox.router.registerRoute(/\.(js|css)(\.map)?$/, staleWhileRevalidate) // js/css
 workbox.router.registerRoute(/\.(ico|png)$/, staleWhileRevalidate) // images
 workbox.router.registerRoute('/hoodie/(.*)', networkOnly)
+
+// third-party
+workbox.router.registerRoute(/https:\/\/raw.githubusercontent.com\/ZeChrales\/PogoAssets\/master\/pokemon_icons\/.+\.png$/, staleWhileRevalidate)
