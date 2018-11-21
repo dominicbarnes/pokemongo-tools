42cd4f20f7cf6eb00a12003c5bd40f081d1e6b8b
Dominic Barnes <dominic@dbarnes.info>
2018-11-21 00:05:08 -0800

    fix(service-worker): workbox@3 updates, precache pokemon 000 icon

--- public/sw.js
+++ public/sw.js
@@ -5,10 +5,14 @@ const networkOnly = workbox.strategies.networkOnly()
 
 workbox.clientsClaim()
 
-workbox.router.registerRoute('/', staleWhileRevalidate) // skeleton
-workbox.router.registerRoute(/\.(js|css)(\.map)?$/, staleWhileRevalidate) // js/css
-workbox.router.registerRoute(/\.(ico|png)$/, staleWhileRevalidate) // images
-workbox.router.registerRoute('/hoodie/(.*)', networkOnly)
+// first-party
+workbox.routing.registerRoute('/', staleWhileRevalidate) // skeleton
+workbox.routing.registerRoute(/\.(js|css)(\.map)?$/, staleWhileRevalidate) // js/css
+workbox.routing.registerRoute(/\.(ico|png)$/, staleWhileRevalidate) // images
+workbox.routing.registerRoute('/hoodie/(.*)', networkOnly)
 
 // third-party
-workbox.router.registerRoute(/https:\/\/raw.githubusercontent.com\/ZeChrales\/PogoAssets\/master\/pokemon_icons\/.+\.png$/, staleWhileRevalidate)
+workbox.precaching.precacheAndRoute([
+  'https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/pokemon_icons/pokemon_icon_000.png'
+])
+workbox.routing.registerRoute(/https:\/\/raw.githubusercontent.com\/ZeChrales\/PogoAssets\/master\/pokemon_icons\/.+\.png$/, staleWhileRevalidate)
