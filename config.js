6beee6cfc3a65eae4dec15034338e5c76ab88bd3
Dominic Barnes <dominic@dbarnes.info>
2019-08-02 02:00:30 -0500

    config: hard-code env configs

--- config.js
+++ config.js
@@ -12,9 +12,15 @@ exports.stage = process.env.UP_STAGE || 'development'
 exports.segmentWriteKey = process.env.SEGMENT_WRITE_KEY
 
 // db
-exports.couchdbURL = process.env.hoodie_dbUrl
-exports.metadataUsername = process.env.METADATA_USERNAME
-exports.metadataPassword = process.env.METADATA_PASSWORD
+if (exports.env === 'development') {
+  exports.couchdbURL = 'http://localhost:5984/'
+  exports.metadataUsername = 'admin'
+  exports.metadataPassword = 'secret'
+} else {
+  exports.couchdbURL = 'https://c38aeb6d-2e18-40ca-b7e9-404da06abd39-bluemix.cloudant.com/'
+  exports.metadataUsername = 'pokemongo-metadata'
+  exports.metadataPassword = 'readonly'
+}
 
 exports.metadataURL = function () {
   const { couchdbURL, metadataUsername, metadataPassword } = exports
